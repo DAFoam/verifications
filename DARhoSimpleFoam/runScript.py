3d8bd2c76a9941d23849053fddaf9bb58a6f6274
Ping He <phe@iastate.edu>
2021-09-03 20:26:50 -0500

    Added DARhoSimpleFoam case.

---
+++ DARhoSimpleFoam/runScript.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+"""
+DAFoam run script to verify the adjoint derivative accuracy
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+import os
+import argparse
+from mpi4py import MPI
+from dafoam import PYDAFOAM, optFuncs
+from pygeo import *
+from pyspline import *
+from idwarp import USMesh
+import numpy as np
+
+
+# =============================================================================
+# Input Parameters
+# =============================================================================
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", help="type of run to do", type=str, default="runAdjoint")
+parser.add_argument("--dvName", help="design variable name for forward AD", type=str, default="shape")
+parser.add_argument("--seedIndex", help="which design variable index to set seeds", type=int, default=0)
+args = parser.parse_args()
+gcomm = MPI.COMM_WORLD
+
+# Define the global parameters here
+U0 = 100.0
+p0 = 101325.0
+T0 = 300.0
+rho0 = 1.178
+nuTilda0 = 4.5e-5
+alpha0 = 3.0
+A0 = 0.1
+
+adjJacOpt = "JacobianFree"
+mode = "reverse"
+
+if args.task == "runForwardAD":
+    adjJacOpt = "JacobianFD"
+    mode = "forward"
+
+# Set the parameters for optimization
+daOptions = {
+    "designSurfaces": ["wing"],
+    "solverName": "DARhoSimpleFoam",
+    "primalMinResTol": 1.0e-14,
+    "adjJacobianOption": adjJacOpt,
+    "useAD": {"mode": mode, "dvName": args.dvName, "seedIndex": args.seedIndex},
+    "primalBC": {
+        "U0": {"variable": "U", "patches": ["inout"], "value": [U0, 0.0, 0.0]},
+        "p0": {"variable": "p", "patches": ["inout"], "value": [p0]},
+        "T0": {"variable": "T", "patches": ["inout"], "value": [T0]},
+        "nuTilda0": {"variable": "nuTilda", "patches": ["inout"], "value": [nuTilda0]},
+        "useWallFunction": True,
+    },
+    "objFunc": {
+        "CD": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "directionMode": "parallelToFlow",
+                "alphaName": "alpha",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0),
+                "addToAdjoint": True,
+            }
+        },
+        "CL": {
+            "part1": {
+                "type": "force",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "directionMode": "normalToFlow",
+                "alphaName": "alpha",
+                "scale": 1.0 / (0.5 * U0 * U0 * A0),
+                "addToAdjoint": True,
+            }
+        },
+        "CMZ": {
+            "part1": {
+                "type": "moment",
+                "source": "patchToFace",
+                "patches": ["wing"],
+                "axis": [0.0, 0.0, 1.0],
+                "center": [0.25, 0.0, 0.05],
+                "scale": 1.0 / (0.5 * U0 * U0 * A0 * 1.0),
+                "addToAdjoint": True,
+            }
+        },
+    },
+    "adjStateOrdering": "cell",
+    "adjEqnOption": {"gmresRelTol": 1.0e-14, "pcFillLevel": 1, "jacMatReOrdering": "rcm"},
+    "normalizeStates": {
+        "U": U0,
+        "p": p0,
+        "nuTilda": nuTilda0 * 10.0,
+        "T": T0,
+        "phi": 1.0,
+    },
+    "adjPartDerivFDStep": {"State": 1e-6},
+    "designVar": {},
+}
+
+# mesh warping parameters, users need to manually specify the symmetry plane and their normals
+meshOptions = {
+    "gridFile": os.getcwd(),
+    "fileType": "OpenFOAM",
+    # point and normal for the symmetry plane
+    "symmetryPlanes": [[[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]], [[0.0, 0.0, 0.1], [0.0, 0.0, 1.0]]],
+}
+
+# =============================================================================
+# Design variable setup
+# =============================================================================
+DVGeo = DVGeometry("./FFD/wingFFD.xyz")
+nTwists = DVGeo.addRefAxis("bodyAxis", xFraction=0.25, alignIndex="k")
+
+
+def alpha(val, geo):
+    aoa = val[0] * np.pi / 180.0
+    inletU = [float(U0 * np.cos(aoa)), float(U0 * np.sin(aoa)), 0]
+    DASolver.setOption("primalBC", {"U0": {"variable": "U", "patches": ["inout"], "value": inletU}})
+    DASolver.updateDAOption()
+
+
+def pitch(val, geo):
+    for i in range(nTwists):
+        geo.rot_z["bodyAxis"].coef[i] = -val[0]
+
+# select points
+iVol = 0
+pts = DVGeo.getLocalIndex(iVol)
+indexList = pts[:, :, :].flatten()
+PS = geo_utils.PointSelect("list", indexList)
+# shape
+DVGeo.addGeoDVLocal("shape", lower=-1.0, upper=1.0, axis="y", scale=1.0, pointSelect=PS)
+daOptions["designVar"]["shape"] = {"designVarType": "FFD"}
+# pitch
+DVGeo.addGeoDVGlobal("pitch", np.zeros(1), pitch, lower=-10.0, upper=10.0, scale=1.0)
+daOptions["designVar"]["pitch"] = {"designVarType": "FFD"}
+# AOA
+DVGeo.addGeoDVGlobal("alpha", value=[alpha0], func=alpha, lower=0.0, upper=10.0, scale=1.0)
+daOptions["designVar"]["alpha"] = {"designVarType": "AOA", "patches": ["inout"], "flowAxis": "x", "normalAxis": "y"}
+# =============================================================================
+# DAFoam initialization
+# =============================================================================
+DASolver = PYDAFOAM(options=daOptions, comm=gcomm)
+DASolver.setDVGeo(DVGeo)
+mesh = USMesh(options=meshOptions, comm=gcomm)
+DASolver.addFamilyGroup(DASolver.getOption("designSurfaceFamily"), DASolver.getOption("designSurfaces"))
+DASolver.printFamilyList()
+DASolver.setMesh(mesh)
+evalFuncs = []
+DASolver.setEvalFuncs(evalFuncs)
+
+# =============================================================================
+# Constraint setup
+# =============================================================================
+DVCon = DVConstraints()
+DVCon.setDVGeo(DVGeo)
+DVCon.setSurface(DASolver.getTriangulatedMeshSurface(groupName=DASolver.getOption("designSurfaceFamily")))
+
+# =============================================================================
+# Initialize optFuncs for optimization
+# =============================================================================
+optFuncs.DASolver = DASolver
+optFuncs.DVGeo = DVGeo
+optFuncs.DVCon = DVCon
+optFuncs.evalFuncs = evalFuncs
+optFuncs.gcomm = gcomm
+
+# =============================================================================
+# Task
+# =============================================================================
+if args.task == "runAdjoint":
+
+    optFuncs.runAdjoint()
+
+elif args.task == "runForwardAD":
+
+    DASolver()
+
+elif args.task == "testAPI":
+
+    DASolver.setOption("primalMinResTol", 1e-2)
+    DASolver.updateDAOption()
+    optFuncs.runPrimal()
+
+else:
+    print("task arg not found!")
+    exit(0)
