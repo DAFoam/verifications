1bc3dc6043b9f21a28c91b89f8d5ba9054468c97
Ping He <phe@iastate.edu>
2022-10-02 13:56:57 -0500

    Update.

---
+++ .testAPI.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+  
+if [ -z "$WM_PROJECT" ]; then
+  echo "OpenFOAM environment not found, forgot to source the OpenFOAM bashrc?"
+  exit 1
+fi
+
+echo "Running.."
+find */runScript* -type f -exec sed -i '/"primalMinResTol"/c\    "primalMinResTol": 0.9,' {} \;
+cd DARhoSimpleCFoam && ./preProcessing.sh && python runScript.py --mode=reverse --task=runAdjoint && python runScript.py --mode=forward --task=runForwardAD --dvName="shape" --seedIndex=0 && cd - || exit 1
+
+
+  
